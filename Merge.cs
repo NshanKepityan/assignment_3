b2cf5f2cf9613d0f9626219f935cdc428c704064
NshanKepityan <36137625+NshanKepityan@users.noreply.github.com>
2018-02-15 01:13:07 +0400

    Update Merge.cs

--- Merge.cs
+++ Merge.cs
@@ -6,67 +6,75 @@ using System.Threading.Tasks;
 
 namespace Merge
 {
+    /// <summary>
+    /// Class containing Merge Sort algorithm implementation.
+    /// </summary>
     class Merge
     {
-        public static void MergeArrays(int[] array, int firstIndex, int middleIndex, int lastindex)
-
+        /// <summary>
+        /// Merges two subarray.
+        /// </summary>
+        /// <param name="arr">The given array to be sorted.</param>
+        /// <param name="firstIndex">The first index of an array.</param>
+        /// <param name="middleIndex">The middle index of an array.</param>
+        /// <param name="lastindex">The last index of an array.</param>
+        public static void MergeArrays(int[] arr, int firstIndex, int middleIndex, int lastIndex)
         {
-
-            int index1 = firstIndex, index2 = middleIndex + 1;
+            int i1 = firstIndex;
+            int i2 = middleIndex + 1;
             int k = 0, j, i;
-            int[] secondArray = new int[lastindex - firstIndex + 1]; 
+            int[] secondArr = new int[lastIndex - firstIndex + 1]; 
 
-
-            for (j = firstIndex; j <= lastindex; j++)
+            for (j = firstIndex; j <= lastIndex; j++)
             {
-                if (index1 > middleIndex)
-                    secondArray[k++] = array[index2++];
+                if (i1 > middleIndex)
+                    secondArr[k++] = arr[i2++];
 
-                else if (index2 > lastindex)
-                    secondArray[k++] = array[index1++];
+                else if (i2 > lastIndex)
+                    secondArr[k++] = arr[i1++];
 
-                else if (array[index1] < array[index2])
-                    secondArray[k++] = array[index1++];
-
-
+                else if (array[i1] < arr[i2])
+                    secondArr[k++] = arr[i1++];
+                
                 else
-                    secondArray[k++] = array[index2++];
+                    secondArr[k++] = arr[i2++];
             }
 
             for (i = 0; i < k; i++)
-
             {
-                array[firstIndex++] = secondArray[i];
+                array[firstIndex++] = secondArr[i];
             }
-
         }
 
 
-
-        public static void TypicalSort(int[] a, int firstIndex, int lastIndex)
+        /// <summary>
+        /// The basic method which implements the Merge Sort algorithm.
+        /// </summary>
+        /// <param name="arr">The given array to be sorted.</param>
+        /// <param name="firstIndex">The first index of an array.</param>
+        /// <param name="lastIndex">The last index of an array.</param>
+        public static void TypicalSort(int[] arr, int firstIndex, int lastIndex)
 
         {
-
             if (firstIndex < lastIndex)
-
             {
                 int midleIndex = (firstIndex + lastIndex) / 2;
-                TypicalSort(a, firstIndex, midleIndex);
-                TypicalSort(a, midleIndex + 1, lastIndex);
-                MergeArrays(a, firstIndex, midleIndex, lastIndex);
-
+                TypicalSort(arr, firstIndex, midleIndex);
+                TypicalSort(arr, midleIndex + 1, lastIndex);
+                MergeArrays(arr, firstIndex, midleIndex, lastIndex);
             }
-
         }
+        /// <summary>
+        /// Sort method wich implements Merge Sort
+        /// </summary>
+        /// <param name="arr">The givent array wich should be sorted</param>
+        /// <returns>Returns sorted array</returns>
         public static int[] Sort(int[] arr)
-
         {
             int[] sorted = arr;
             TypicalSort(arr, 0, arr.Length - 1);
             return sorted;
         }
-
-
     }
 }
 
